e596cb21f975cff0b0b0b2641d3104c48f685b07
jme <jinux@alluha.net>
2015-02-23 21:14:00 +0000

    Removed old comments.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -11,27 +11,22 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-    // Get the NewPerson function.
     Module_loader* loadModule = (Module_loader*)dlsym(module_so, "loadModule");
     if (!loadModule) {
         exit(EXIT_FAILURE);
     }
 
-    // Get the DeletePerson function.
     Module_unloader* unloadModule =
         (Module_unloader*)dlsym(module_so, "unloadModule");
     if (!unloadModule) {
         exit(EXIT_FAILURE);
     }
 
-    // Create Person objects.
     Module* mod = loadModule();
     std::cout << mod->name() << std::endl;
 
-    // Destroy Person objects.
   	unloadModule(mod);
 
-    // Close the library.
     if (dlclose(module_so) != 0) {
         exit(EXIT_FAILURE);
     }
