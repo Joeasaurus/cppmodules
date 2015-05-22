7e924f2fc8ee6ddcc95688a3e88398783284a307
Joe Eaves @shinsei <jinux@alluha.net>
2015-05-22 13:06:11 +0100

    Better logging and comments

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -4,18 +4,26 @@
 #include "lib/spdlog/spdlog.h"
 
 int main(int argc, char **argv) {
+	// Configure the global logger
     spdlog::set_async_mode(1048576); //queue size must be power of 2
     spdlog::set_level(spdlog::level::info);
 
+    // Create a stdout logger, multi threaded
 	std::shared_ptr<spdlog::logger> logger = spdlog::stdout_logger_mt("Logger");
-	logger->info("Main started, Logger up");
+	logger->debug("{}: {}", "Main", "Logger initialised");
 
+	// Open the spine and pass it out logger
 	Spine *spine = new Spine;
 	spine->setLogger(logger);
+
+	// Now we create our ZMQ context for all our sockets
 	zmq::context_t spineCtx(1);
 
 	try {
+		// Lets pass the context through and use it to open all out sockets
+		// Then we subscribe the spine to the spine channel
 		spine->setSocketContext(&spineCtx);
+		logger->debug("{}: {}", "Main", "Spine open, ZMQ context set");
 		spine->openSockets();
 		spine->subscribe(spine->name());
 	} catch (const zmq::error_t &e) {
@@ -23,7 +31,7 @@ int main(int argc, char **argv) {
  		return 1;
 	}
 
-	logger->info("Loading spine modules");
+	// Load all the modules we wrote
 	spine->loadModules("./modules");
 	logger->info("{}: {}", "Main", "Running spine");
 	bool spineReturn = spine->run();
