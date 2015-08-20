7e9cfa4e4b7192a61570443c667f5a711627cc2f
Joe Eaves @shinsei <jinux@alluha.net>
2015-08-20 19:06:15 +0100

    Improved tests, some now use BDD!
    There is a bug at the moment though: SpineAPI will hang if you run it multiple times in quick succession. No idea why.

--- src/tests/CreateSpineLogger.cpp
+++ src/tests/CreateSpineLogger.cpp
@@ -1,19 +1,32 @@
-#define CATCH_CONFIG_RUNNER
-#include "lib/catch/catch.hpp"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 #include "main/spine.hpp"
 
 static std::string LoggerName = "SpineLogger";
 
-TEST_CASE( "SpineLogger is created", "[logger]" ) {
-	REQUIRE(spdlog::get(LoggerName)!=nullptr);
-	REQUIRE_THROWS_AS(spdlog::stdout_logger_mt(LoggerName), spdlog::spdlog_ex);
-}
+SCENARIO("Spine creates a valid logger named 'SpineLogger'", "[spine,logger]") {
+	GIVEN("the logger is created") {
+		auto logger = Spine::createLogger(false);
 
-int main(int argc, char* const argv[]) {
-	// Create the logger we will test later
-	auto logger = Spine::createLogger(argc > 1 && strcmp(argv[1], "debug") == 0);
+		WHEN("the logger is retrieved") {
+			auto gotLogger = spdlog::get(LoggerName);
+			THEN("the logger can be retrieved from the registry") {
+				REQUIRE(gotLogger != nullptr);
+			}
+		}
 
-	// run tests
-	return Catch::Session().run(argc, argv);
+		WHEN("we try to create the logger again") {
+			auto secondLogger = Spine::createLogger(false);
+			THEN("we receive the same logger from the registry") {
+				REQUIRE(logger == secondLogger);
+			}
+		}
+
+		WHEN("we try to create the same logger directly") {
+			THEN("an exception is thrown") {
+				REQUIRE_THROWS_AS(spdlog::stdout_logger_mt(LoggerName), spdlog::spdlog_ex);
+			}
+		}
+	}
 }
--- src/tests/SpineAPI.cpp
+++ src/tests/SpineAPI.cpp
@@ -1,49 +1,41 @@
 #define CATCH_CONFIG_RUNNER
-#include "lib/catch/catch.hpp"
+#include "catch.hpp"
 
 #include "main/spine.hpp"
-#include "main/segvhandler.hpp"
 
 static std::string LoggerName = "SpineLogger";
 
 class SpineTestsFixture {
 	private:
 		Spine spine;
-	protected:
-		bool CreateSpine() {
-			return SpineOpenSockets();
+	public:
+		SpineTestsFixture() {
+			REQUIRE(SpineOpenSockets());
 		}
+	protected:
 		bool SpineOpenSockets() {
 			REQUIRE_NOTHROW(spine.openSockets());
 			REQUIRE(spine.areSocketsValid());
 			return spine.areSocketsOpen();
 		}
+		bool SpineLoadModules() {
+			return spine.loadModules(spine.moduleFileLocation);
+		}
 		bool SpineDefaultModulesLoaded() {
-			spine.loadModules(spine.moduleFileLocation);
-			REQUIRE(spine.isModuleLoaded("mainline_config"));
-			return true;
+			return spine.isModuleLoaded("mainline_config");
 		}
  };
 
-TEST_CASE_METHOD(SpineTestsFixture, "Spine functions", "[spine]") {
-	SECTION("Spine is created") {
-		REQUIRE(CreateSpine());
-	}
-	SECTION("Spone loads default modules") {
-		REQUIRE(CreateSpine());
+TEST_CASE_METHOD(SpineTestsFixture, "All", "[spine]") {
+	SECTION("Spine loads default modules") {
+		REQUIRE(SpineLoadModules());
 		REQUIRE(SpineDefaultModulesLoaded());
 	}
 }
 
-TEST_CASE( "SpineLogger is created", "[logger]" ) {
-	REQUIRE(spdlog::get(LoggerName) != nullptr);
-	REQUIRE_THROWS_AS(spdlog::stdout_logger_mt(LoggerName), spdlog::spdlog_ex);
-}
-
 int main(int argc, char* const argv[]) {
 	// Create the logger we will test later
-	auto logger = Spine::createLogger(argc > 1 && strcmp(argv[1], "debug") == 0);
-
+	auto logger = Spine::createLogger(false);
 	// run tests
 	return Catch::Session().run(argc, argv);
 }
