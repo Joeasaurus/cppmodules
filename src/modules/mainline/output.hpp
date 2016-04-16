9aa1f0b03481dd4469125fe0d4d093bec56f2a27
Joe Eaves @Soul <jinux@alluha.net>
2016-04-16 20:06:58 +0100

    Added OutputModule to simulate data on CHANNEL::Out.

---
+++ src/modules/mainline/output.hpp
@@ -0,0 +1,26 @@
+#pragma once
+// Common
+#include "main/module.hpp"
+#include "Eventer.hpp"
+
+using namespace cppm;
+using namespace cppm::messages;
+using namespace cppevent;
+
+class OutputModule : public Module {
+	public:		
+		OutputModule() : Module("output", "Joe Eaves"){};
+		~OutputModule();
+		void setup();
+		void tick();
+		bool process_command(const Message& message){return true;};
+		bool process_input  (const Message& message){return true;};
+	private:
+		Eventer _eventer;
+		Output message{"output"};
+
+};
+
+// Init/Del functions.
+extern "C" OutputModule* createModule();
+extern "C" void destroyModule(OutputModule* module);
